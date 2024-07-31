29a38f274b2dbf86487918658d75303e10dce11c
Hyeongu <rkdgusrndla@gmail.com>
2024-07-31 14:03:41 +0900

    d

--- src/main/java/com/kkodamkkodam/util/filter/AuthenticationFilter.java
+++ src/main/java/com/kkodamkkodam/util/filter/AuthenticationFilter.java
@@ -19,7 +19,18 @@ import jakarta.servlet.http.HttpSession;
 @WebFilter({"/user/mypage.user", 
 			"/user/modify.user", 
 			"/user/delete.user", 
-			"/user/delete_check.user"})
+			"/user/delete_check.user",
+			"/board/post_write.board",
+			"/board/post_regi.board",
+			"/board/post_code_write.board",
+			"/board/post_code_regi.board",
+			"/board/updatePostPage.board",
+			"/board/updatePost.board",
+			"/board/deletePost.board",
+			"/board/commentWrite.board",
+			"/board/replyWrite.board",
+			"/board/deleteComment.board",
+			"/board/increaseCommentLike.board"})
 public class AuthenticationFilter implements Filter{
 
 	@Override
@@ -33,15 +44,11 @@ public class AuthenticationFilter implements Filter{
         UserDTO user = (UserDTO)session.getAttribute("user");
         
         if (user == null) {
-        	// 로그인 되지 않은 상태
-        	httpResponse.setContentType("text/html; charset=UTF-8;");
-        	PrintWriter out = response.getWriter();
-        	out.println("<script>");
-        	out.println("alert('로그인 후 이용해 주세요.');");
-        	out.println("location.href='"+ httpRequest.getContextPath()+"/user/login.user';"); // 로그인 페이지 경로로 수정
-        	out.println("</script>");
-        	return;
+            // 로그인 되지 않은 상태
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/user/login.user"); // 로그인 페이지로 리다이렉트
+            return;
         }
+        
         
         // 로그인 된 상태
         chain.doFilter(request, response);
